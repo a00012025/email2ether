2b85140d95eaa2a8b9b0c723e9db400110a5959f
Harry Chen <a00012025@gmail.com>
2024-02-26 11:06:11 -0700

    fix: update gitignore

--- backend/main.go
+++ backend/main.go
@@ -20,7 +20,7 @@ func getClient(config *oauth2.Config) *http.Client {
 	// The file token.json stores the user's access and refresh tokens, and is
 	// created automatically when the authorization flow completes for the first
 	// time.
-	tokFile := "token.json"
+	tokFile := "credentials/token.json"
 	tok, err := tokenFromFile(tokFile)
 	if err != nil {
 		tok = getTokenFromWeb(config)
@@ -87,7 +87,7 @@ func GetEmail(srv *gmail.Service, messageId string) {
 
 func main() {
 	ctx := context.Background()
-	b, err := os.ReadFile("credentials.json")
+	b, err := os.ReadFile("credentials/credentials.json")
 	if err != nil {
 		log.Fatalf("Unable to read client secret file: %v", err)
 	}
