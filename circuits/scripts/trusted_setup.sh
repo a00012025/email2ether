457ec138153343f59bf7dbd173e07404c6092fbb
Harry Chen <a00012025@gmail.com>
2024-02-27 12:03:56 -0700

    fix: update trusted setup and max header len

--- circuits/scripts/trusted_setup.sh
+++ circuits/scripts/trusted_setup.sh
@@ -5,18 +5,12 @@ set -e
 mkdir -p setup
 
 # phase 1
-if [ ! -f setup/pot22_0000.ptau ]; then
-  snarkjs powersoftau new bn128 22 setup/pot22_0000.ptau
-fi
-if [ ! -f setup/pot22_0001.ptau ]; then
-  snarkjs powersoftau contribute setup/pot22_0000.ptau setup/pot22_0001.ptau --name="First contribution" -e="ds982JDiubv2"
-fi
-if [ ! -f setup/pot22_final.ptau ]; then
-  snarkjs powersoftau prepare phase2 setup/pot22_0001.ptau setup/pot22_final.ptau
+if [ ! -f setup/powersOfTau28_hez_final_22.ptau ]; then
+  curl https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_22.ptau -o setup/powersOfTau28_hez_final_22.ptau
 fi
 
 # phase 2
-snarkjs groth16 setup build/email_change_owner.r1cs setup/pot22_final.ptau setup/email_change_owner_0000.zkey
+snarkjs groth16 setup build/email_change_owner.r1cs setup/powersOfTau28_hez_final_22.ptau setup/email_change_owner_0000.zkey
 snarkjs zkey contribute setup/email_change_owner_0000.zkey setup/email_change_owner_0001.zkey --name="1st Contributor Name" -e="v93iUFDIU87t3vl"
 
 # export
