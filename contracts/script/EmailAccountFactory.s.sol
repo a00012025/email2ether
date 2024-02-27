fc1dcfc46aad3d776789a240ee4ee69f4f6d2bfc
kuan0808 <pp0923215826@gmail.com>
2024-02-26 18:04:01 -0700

    feat(contracts): erc 4337 email account implementation in progress

---
+++ contracts/script/EmailAccountFactory.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "../src/email-account/EmailAccountFactory.sol";
+
+contract Deployer is Script {
+    uint256 private deployerPrivateKey;
+    address private entryPoint = 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
+
+
+
+    function setup() external {
+        deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    }
+
+    function run() external {
+        vm.startBroadcast(deployerPrivateKey);
+
+        EmailAccountFactory factory = new EmailAccountFactory(IEntryPoint(entryPoint));
+
+        vm.stopBroadcast();
+    }
+}
