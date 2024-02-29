2fd13a288c07c5cd97546c0d165aa5a8c5adc83d
fourcolors <sterlingcobb@gmail.com>
2024-02-28 20:25:49 -0700

    setup viem client and local storage

---
+++ frontend/src/lib/wallet.ts
@@ -0,0 +1,19 @@
+import { Account, Client, createWalletClient, http } from "viem";
+import { arbitrumSepolia } from "viem/chains";
+
+let client: Client | null = null;
+function createClientFromAccount(account: Account) {
+  if (!client) {
+    client = createWalletClient({
+      account,
+      chain: arbitrumSepolia,
+      transport: http(
+        "https://arb-sepolia.g.alchemy.com/v2/jDR80Y23Gv4tPFHxHpPVwOK6PE4_a3kL"
+      ),
+    });
+  }
+  return client;
+}
+
+export { createClientFromAccount };
+export default client;
---
+++ frontend/src/store/persistent.ts
@@ -0,0 +1,40 @@
+import { Account } from "viem";
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface StoreState {
+  account: Account | null;
+  email: string | null;
+}
+
+const initialState: StoreState = {
+  account: null,
+  email: null,
+};
+
+type StoreFunctions = {
+  setupNewAccount: () => void;
+  setEmail: (email: string) => void;
+};
+
+export const useStore = create<StoreState & StoreFunctions>()(
+  persist(
+    (set, get) => ({
+      ...initialState,
+      setupNewAccount: () => {
+        if (get().account === null) {
+          const privateKey = generatePrivateKey();
+          set({ account: privateKeyToAccount(privateKey) });
+        }
+      },
+      setEmail: (email: string) => {
+        set({ email });
+      },
+    }),
+    {
+      name: "persistent-user-account-storage",
+      getStorage: () => localStorage,
+    }
+  )
+);
