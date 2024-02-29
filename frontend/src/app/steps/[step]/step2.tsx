a3798b1df5f97804322628cf90443a41598f743a
fourcolors <sterlingcobb@gmail.com>
2024-02-28 21:41:19 -0700

    add loading

--- frontend/src/app/steps/[step]/step2.tsx
+++ frontend/src/app/steps/[step]/step2.tsx
@@ -1,19 +1,36 @@
 "use client";
 
 import AnimatedButton from "@/components/Button";
+import { motion } from "framer-motion";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 
-export default function VerifyEmail() {
-  const [email, setEmail] = useState("");
+const Loading = ({ title }: { title: string }) => {
+  return (
+    <div className="flex flex-col items-center gap-4 h-full justify-center">
+      <div className="text-xl font-bold">{title}</div>
+      <motion.div
+        animate={{ rotate: 360 }}
+        transition={{ repeat: Infinity, ease: "linear", duration: 1 }}
+        style={{
+          width: "40px",
+          height: "40px",
+          borderRadius: "50%",
+          border: "5px solid lightgray",
+          borderTop: "5px solid magenta",
+        }}
+      />
+    </div>
+  );
+};
+
+interface MainContentProps {
+  onSubmit: (data: any) => void;
+  handleClick: () => void;
+}
+
+function MainContent({ onSubmit, handleClick }: MainContentProps) {
   const { register, handleSubmit } = useForm(); // Assuming useForm is already defined
-  const onSubmit = (data) => console.log(data);
-
-  function handleClick() {
-    window.location.href = "mailto:test@example.com";
-    console.log("Clicked");
-  }
-
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
@@ -28,4 +45,17 @@ export default function VerifyEmail() {
       </div>
     </form>
   );
+}
+
+export default function VerifyEmail() {
+  const [email, setEmail] = useState("");
+  const { register, handleSubmit } = useForm(); // Assuming useForm is already defined
+  const onSubmit = (data) => console.log(data);
+
+  function handleClick() {
+    window.location.href = "mailto:test@example.com";
+    console.log("Clicked");
+  }
+
+  return <Loading title="Finding your account..." />;
 }
