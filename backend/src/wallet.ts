228bcf2672688b76900c5e57791cac5a4edcf295
Harry Chen <a00012025@gmail.com>
2024-03-01 15:23:49 -0700

    feat: multi chain

--- backend/src/wallet.ts
+++ backend/src/wallet.ts
@@ -3,18 +3,40 @@ import {
   http,
   getContract,
   createWalletClient,
+  Chain,
 } from "viem";
 import EmailAccountAbi from "./abi/EmailAccount";
-import { arbitrumSepolia } from "viem/chains";
+import {
+  arbitrumSepolia,
+  polygonMumbai,
+  baseSepolia,
+  lineaTestnet,
+} from "viem/chains";
 import { privateKeyToAccount } from "viem/accounts";
 import EmailAccountFactoryAbi from "./abi/EmailAccountFactory";
 require("dotenv").config();
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const RPC_URL = process.env.RPC_URL;
-
-if (!PRIVATE_KEY || !RPC_URL) {
-  throw new Error("PRIVATE_KEY or RPC_URL env variable is missing");
+const CHAIN_ID = process.env.CHAIN_ID;
+if (!PRIVATE_KEY || !CHAIN_ID) {
+  throw new Error("PRIVATE_KEY or RPC_URL or CHAIN_ID env variable is missing");
+}
+let network: Chain;
+let accountFactoryContractAddr: string;
+if (CHAIN_ID === "42161") {
+  network = arbitrumSepolia;
+  accountFactoryContractAddr = "0x763c0B996E6C931e828974b87Dcf455c0F3D49e7";
+} else if (CHAIN_ID === "80001") {
+  network = polygonMumbai;
+  accountFactoryContractAddr = "0xD570bF4598D3ccF214E288dd92222b8Bd3134984";
+} else if (CHAIN_ID === "84532") {
+  network = baseSepolia;
+  accountFactoryContractAddr = "";
+} else if (CHAIN_ID === "59140") {
+  network = lineaTestnet;
+  accountFactoryContractAddr = "";
+} else {
+  throw new Error("Invalid CHAIN_ID");
 }
 
 export const walletClient = createWalletClient({
@@ -24,7 +46,7 @@ export const walletClient = createWalletClient({
 });
 
 const accountFactory = getContract({
-  address: process.env.ACCOUNT_FACTORY_CONTRACT_ADDRESS! as `0x${string}`,
+  address: accountFactoryContractAddr as `0x${string}`,
   abi: EmailAccountFactoryAbi,
   client: walletClient,
 });
@@ -33,7 +55,7 @@ let currentNonce: number;
 
 export async function initWallet() {
   const publicClient = createPublicClient({
-    chain: arbitrumSepolia,
+    chain: network,
     transport: http(),
   });
   currentNonce = await publicClient.getTransactionCount({
