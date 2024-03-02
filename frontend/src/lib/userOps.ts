b9f397c1da8bcb514f888c6aa4789f78cd0b3234
kuan0808 <pp0923215826@gmail.com>
2024-03-02 03:05:23 -0700

    feat(frontend): add userOp related functions to mint demo NFT

---
+++ frontend/src/lib/userOps.ts
@@ -0,0 +1,90 @@
+import { getUserOpHash } from "@account-abstraction/utils";
+import {
+  createWalletClient,
+  encodeFunctionData,
+  getContract,
+  http,
+} from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { arbitrumSepolia } from "viem/chains";
+
+import demoNftAbi from "../constants/DemoNftAbi.json";
+import emailAccountAbi from "../constants/EmailAccountAbi";
+import entryPointAbi from "../constants/EntryPointAbi";
+
+const DEMO_NFT_ADDRESS = "0x31055ED712eE29841f09D5F9a46964581829f15e";
+const ENTRYPOINT_ADDRESS = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
+const PAYMASTER_ADDRESS = "0xFB0AD3C188DC1D3D490C4e00aBF261Aa5613a25b";
+const CHAIN_ID = "421614";
+
+export const generateNftMintingCalldata = (
+  recipient: string,
+  imgIdx: number
+) => {
+  const data = encodeFunctionData({
+    abi: demoNftAbi.abi,
+    functionName: "mint",
+    args: [recipient, imgIdx],
+  });
+
+  const calldata = encodeFunctionData({
+    abi: emailAccountAbi,
+    functionName: "execute",
+    args: [DEMO_NFT_ADDRESS, 0n, data],
+  });
+
+  return calldata;
+};
+
+const getCurrentNonce = async (address: string) => {
+  const walletClient = createWalletClient({
+    chain: arbitrumSepolia,
+    transport: http(),
+  });
+  const entrypoint = getContract({
+    address: ENTRYPOINT_ADDRESS,
+    abi: entryPointAbi,
+    client: walletClient,
+  });
+  const nonce = await entrypoint.read.nonceSequenceNumber([
+    address as `0x${string}`,
+    0n,
+  ]);
+  return nonce;
+};
+
+export const generateUserOps = async (sender: string, calldata: string) => {
+  const nonce = await getCurrentNonce(sender);
+
+  return {
+    sender,
+    nonce,
+    calldata,
+    callGasLimit: 200000n,
+    verificationGasLimit: 100000n,
+    preVerificationGas: 100000n,
+    maxFeePerGas: BigInt(2e8),
+    maxPriorityFeePerGas: BigInt(2e8),
+    paymasterAndData: PAYMASTER_ADDRESS,
+  };
+};
+
+export const signUserOps = async (userOp: any, privateKey: string) => {
+  const userOpHash = getUserOpHash(
+    userOp,
+    ENTRYPOINT_ADDRESS,
+    Number.parseInt(CHAIN_ID)
+  );
+  console.log("userOpHash", userOpHash);
+
+  userOp.signature = (await privateKeyToAccount(
+    privateKey as `0x${string}`
+  ).signMessage({
+    message: {
+      raw: userOpHash as `0x${string}`,
+    },
+  })) as `0x${string}`;
+  console.log("Full user op:", userOp);
+
+  return userOp;
+};
