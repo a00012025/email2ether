2cd091136bf9353b4d0d5a3ec9eb2dd7478026c7
kuan0808 <pp0923215826@gmail.com>
2024-03-01 21:04:58 -0700

    feat: update the demo nft contract

--- contracts/src/DemoDenverNft.sol
+++ contracts/src/DemoDenverNft.sol
@@ -4,11 +4,37 @@ pragma solidity ^0.8.12;
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 
 contract DemoDenverNft is ERC721 {
-    constructor() ERC721('Email2Ether Denver', 'E2E-DENVER-NFT') {
-        _mint(msg.sender, 0);
+    using Strings for uint256;
+    string public baseURI;
+    uint256 _nextTokenId;
+
+    constructor(string memory anBaseURI) ERC721('Email2Ether Denver', 'E2E-DENVER') {
+        baseURI = anBaseURI;
     }
 
-    function tokenURI(uint256 /* tokenId */) public pure override returns (string memory) {
-        return 'https://email2ether.com/denver-nft';
+    mapping(address => bool) public hasMinted;
+    mapping(uint256 => uint256) public tokenIdToImgIdx;
+
+    function mint(address to, uint256 imgIdx) public {
+        require(!hasMinted[to], 'You have already minted');
+        require(imgIdx < 6, 'Invalid imgIdx');
+
+        hasMinted[to] = true;
+        _nextTokenId++;
+        tokenIdToImgIdx[_nextTokenId] = imgIdx;
+
+        _safeMint(to, _nextTokenId);
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI; 
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        _requireOwned(tokenId);
+
+        uint256 imgIdx = tokenIdToImgIdx[tokenId];
+
+        return string.concat(baseURI, imgIdx.toString());
     }
 }
